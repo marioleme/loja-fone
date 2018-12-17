2e5b7dba47a77c52569b5e591508a003e9739d47
GarciaDFE <garcia.dfe@gmail.com>
2018-12-17 12:10:07 -0200

    [013] - Desafio básico

---
+++ js/button-store.js
@@ -0,0 +1,10 @@
+/* Desafios da Aula 013:
+Básico - Mostrar o HTML do button "Adicionar no Carrinho" no console
+Steps:
+1 - pegar a janela -> o HTML -> e o button e colocar em uma variável constante
+2 - mostrar o conteúdo dessa variável no console
+*/
+
+const $buttonCar = window.document.querySelector(".-second");
+
+console.log($buttonCar);
