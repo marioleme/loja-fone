35333de39097cf2ecf0ab5cba308d9ba6c4466dd
GarciaDFE <garcia.dfe@gmail.com>
2018-12-18 19:01:15 -0200

    [014] - Desafio intermediário

--- js/product-store.js
+++ js/product-store.js
@@ -6,3 +6,13 @@ $heart.addEventListener("click", handleClick);
 function handleClick() {
     $heart.classList.toggle("-active");
 }
+
+/* ESTRELAS -- RANK */
+const $star = document.querySelectorAll(".item.-star .icon")
+
+for (let i = 0; i <= $star.length; i++) {
+    $star[i].addEventListener("click", handleClick);
+    function handleClick() {
+        $star[i].classList.toggle("-active");
+    }
+}
