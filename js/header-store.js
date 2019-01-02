13e44c7d805aced6c648fd547945a98b0705fa4f
GarciaDFE <garcia.dfe@gmail.com>
2019-01-02 17:23:56 -0200

    [017] - Desafio avançado

--- js/header-store.js
+++ js/header-store.js
@@ -1,5 +1,7 @@
 const $search = document.querySelector(".formSearch")
 const $imgButton = $search.querySelector(".icon");
 
-/* simplificando em uma linha a arrow-function + comando de uma linha apenas */
-$imgButton.addEventListener("click", () => $search.classList.toggle("-active"));
+$imgButton.addEventListener("click", () => {
+    $search.classList.toggle("-active")
+    $imgButton.classList.toggle("-close")
+});
